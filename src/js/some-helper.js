a6916844f197d211bd6bd8a20f4a20fbf38a8345
Robert Laws <roblaws@me.com>
2019-06-17 09:56:00 +0300

    add every and some array helpers

---
+++ src/js/some-helper.js
@@ -0,0 +1,14 @@
+let someOne = document.querySelector("#some-result-one");
+
+const computers2 = [
+  { name: "Apple", ram: 24 },
+  { name: "Compaq", ram: 4 },
+  { name: "Dell", ram: 32 },
+  { name: "Acer", ram: 16 }
+]
+
+let someRun = computers2.some(function(computer) {
+  return computer.ram > 16;
+});
+
+someOne.innerHTML = `<p>${someRun}</p>`;
